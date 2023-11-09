d4850c7b0a7d510a73b7f088b6606df503141664
rhit-chungea <chungea@rose-hulman.edu>
2023-11-08 19:59:36 -0500

    backend works

--- exam_2/webServer/app.js
+++ exam_2/webServer/app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
-app.use('/', express.static("public"));
-app.use('/api/', express.json());
+app.use(express.static("public"));
+app.use(express.json());
 
 let data = {};
 // let data = [];  // or an empty array depending on the instructions for your specific exam
@@ -28,8 +28,42 @@ function saveToServer(data) {
     })
 }
 
+async function readMonths(){
+    try{
+        const data = fs.readFileSync(serverSideStorage, 'utf-8');
+        return JSON.parse(data);
+    } catch(error){
+        console.error('Error reading months:', error);
+        return [];
+    }
+}
+
 
 // TODO: Create your backend API here:
+app.get('/getmonths', async (req, res) =>{
+    const months = await readMonths();
+    res.json(months);
+});
 
+
+app.post('/setmonths', async (req, res) => {
+    const { months } = req.body;
+    const month = await readMonths();
+    const newMonth = { months };
+    month.parse;
+    saveToServer(months); //Post by using this syntax for json: {"months": ["jan", "feb"]}
+    res.status(201).json(newMonth);
+});
+
+app.put('/move/:fromindex/:toindex', async (req, res) =>{
+    
+    const { fromindex, toindex } = req.params;
+    const months = await readMonths();
+    const monthMoved = months.splice(fromindex, 1)[0]; // Does the splice and returns the month
+    months.splice(toindex, 0, monthMoved);  // Inserts the month back into the array at the index
+
+    saveToServer(months);
+    res.status(201).json(months)
+});
 
 app.listen(3000);
